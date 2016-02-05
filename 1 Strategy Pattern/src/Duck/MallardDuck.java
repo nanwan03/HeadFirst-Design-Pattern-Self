664bb62ff3854c5fe5a1b8f340f7d15c85e27350
nanwan03 <wn1842@gmail.com>
2016-02-04 22:35:31 -0500

    commit

---
+++ 1 Strategy Pattern/src/Duck/MallardDuck.java
@@ -0,0 +1,18 @@
+package Duck;
+
+import FlyBehavior.FlyWithWings;
+import QuackBehavior.Quack;
+
+public class MallardDuck extends Duck {
+
+	public MallardDuck() {
+		quackBehavior = new Quack();
+		flyBehavior = new FlyWithWings();
+	}
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("I'm a real Mallard duck");
+	}
+
+}
---
+++ 1 Strategy Pattern/src/FlyBehavior/FlyWithWings.java
@@ -0,0 +1,11 @@
+package FlyBehavior;
+
+public class FlyWithWings implements FlyBehavior {
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		System.out.println("I'm flying!!");
+	}
+
+}
